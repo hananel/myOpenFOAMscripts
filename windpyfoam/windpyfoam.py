9e4e756d429cafcab28bde47f4ec78a60e8b4ab8
Hanan (220) Einav Levy <levy.hanan@gmail.com>
2012-11-22 15:36:42 +0200

    windpyfoam.py: partially working

--- windpyfoam/windpyfoam.py
+++ windpyfoam/windpyfoam.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """
 
 windpyfoam
@@ -12,10 +13,16 @@ Read the following from windPyFoamDict (OpenFOAM dictionary file):
  name of stl file - string
  is stl file a rectangular flat edge outline - boolean
  check mesh convergance - boolean
- direction distribution - vector of floating point angles in degree units
+ direction distribution - vector<(weight, angle)> floating point, angle in degrees. 0 <= weight <= 1
 
 Ctrl-C stops calculation
 Display progress to stdout
+
+Next step:
+GUI for same
+
+Other parameters:
+how many processors to use (should be <= multiprocessing.cpu_count())
 
 """
 
@@ -27,7 +34,7 @@ Start_with_case_file -> load terrain file - STL -> <is it big enough and ends on
 Q1 yes -> use SnappyHexMeshDict1.template -> C1
 Q1 no -> use SnappyHexMeshDict2.template -> C1
 
-C1 -> produce series of cases -> Loop1
+C1 -> copy selected template to ____ -> produce series of cases -> Loop1
 
 Loop1 -> chance BC -> run RoughnessToFoam -> run case -> sample and process results{E} -> Loop1
 
@@ -36,31 +43,332 @@ E -> produce report{P}
 P ? compare to measurements ?
 """
 
+"""
+Where is everything?
+
+from PyFoam.Basics.TemplateFile         import TemplateFile
+template = TemplateFile(selected_template_file)
+template.writeToFile(temp_file_name, {parameters from windFile})
+
+run3dHillBase contains the BC (Boundary Condition) code (run3dHillBase.py:run3dHillBase).
+ - read from template 
+
+"""
+
+"""
+TODO:
+we changed inputDict["SHMParams"]["cellSize"]["cellYfactor"] to cell_size from wind_dict, is that wise?
+"""
+
 import sys
 import os
+from os import path
+from math import pi, sin, cos, floor, log, sqrt
+import multiprocessing
+import itertools
 from argparse import ArgumentParser
-from PyFoam.RunDictionary.ParsedParameterFile import ParsedParameterFile
 
-def main(case):
-    if not os.path.exists(case):
-        print "case directory does not exist"
-        raise SystemExit
-    if not os.path.isdir(case):
-        print "case is not a directory"
-        raise SystemExit
-    wind_dict = os.path.join(case, 'windPyFoamDict')
-    if not os.path.exists(wind_dict):
-        print "missing %s file" % wind_dict
+from PyFoam.RunDictionary.SolutionDirectory     import SolutionDirectory
+from PyFoam.RunDictionary.ParsedParameterFile import ParsedParameterFile
+from PyFoam.Basics.TemplateFile         import TemplateFile
+from PyFoam.Applications.Decomposer import Decomposer
+from PyFoam.Execution.BasicRunner 		import BasicRunner
+
+sys.path.append('../')
+from runCases import runCasesFiles as runCases
+
+def warn(x):
+    print "WARNING:", x
+
+def debug(x):
+    print "DEBUG:", x
+
+def status(x):
+    print "STATUS:", x
+
+def create_block_mesh_dict(work, wind_dict, params):
+    phi = params['phi']
+    cell_size = params['cell_size']
+    SHM = wind_dict["SHMParams"]
+    Href = SHM["domainSize"]["domZ"]
+    domainSize = SHM["domainSize"]
+    l, d = domainSize["fX"], domainSize["fY"]
+    cell = SHM["cellSize"]["cell"]
+    x0, y0 = (SHM["centerOfDomain"]["x0"],
+              SHM["centerOfDomain"]["x0"])
+    sin_phi = sin(phi)
+    cos_phi = cos(phi)
+    x1 = x0 - (l / 2 * sin_phi + d / 2 * cos_phi)
+    y1 = y0 - (l / 2 * cos_phi - d / 2 * sin_phi)
+    x2 = x0 - (l / 2 * sin_phi - d / 2 * cos_phi)
+    y2 = y0 - (l / 2 * cos_phi + d / 2 * sin_phi)
+    x3 = x0 + (l / 2 * sin_phi + d / 2 * cos_phi)
+    y3 = y0 + (l / 2 * cos_phi - d / 2 * sin_phi)
+    x4 = x0 + (l / 2 * sin_phi - d / 2 * cos_phi)
+    y4 = y0 + (l / 2 * cos_phi + d / 2 * sin_phi)
+    n = floor(d / (cell * cell_size))
+    m = floor(l / (cell * cell_size))
+    q = floor((Href + 450.0) / cell) # -450 is the minimum of the
+                                     # blockMeshDict.template - since that is
+                                     # slightly lower then the lowest point
+                                     # on the planet
+    if n == 0 or m == 0 or q == 0:
+        print "invalid input to block mesh dict:"
+        print "d = %(d)f, l = %(l)f, Href = %(Href)f, cell = %(cell)f, cell_size = %(cell_size)f" % locals()
+        print "n = %(n)f, m = %(m)f, q = %(q)f" % locals()
+    assert(n > 0 and m > 0 and q > 0)
+    bmName = path.join(work.constantDir(),"polyMesh/blockMeshDict")
+    template = TemplateFile(bmName+".template")
+    template.writeToFile(bmName,
+        {'X0':x1,'X1':x2,'X2':x3,'X3':x4,
+         'Y0':y1,'Y1':y2,'Y2':y3,'Y3':y4,
+         'Z0':Href,'n':int(n),'m':int(m),'q':int(q)})
+
+def create_SHM_dict(work, wind_dict, params):
+    print "calculating SHM parameters"
+    phi = params['phi']
+    SHM = wind_dict["SHMParams"]
+    domainSize = SHM['domainSize']
+    a = domainSize['refinement_length']
+    H = domainSize['typical_height']
+    cell = SHM["cellSize"]["cell"]
+    zz = SHM["pointInDomain"]["zz"]
+    x0, y0 = (SHM["centerOfDomain"]["x0"],
+              SHM["centerOfDomain"]["x0"])
+    z0 = wind_dict['simParams']['z0'] # TODO: calculated per wind direction using roughness2foam
+    # calculating refinement box positions
+    l1, l2, h1, h2 = 2*a, 1.3*a, 4*H, 2*H # refinement rules - Martinez 2011
+    sp = sin(phi)
+    cp = cos(phi)
+    refBox1_minx, refBox1_miny, refBox1_minz = \
+        x0 - l1*(sp+cp), y0 - l1*(cp-sp), 0 #enlarging to take acount of the rotation angle
+    refBox1_maxx, refBox1_maxy, refBox1_maxz = \
+        x0 + l1*(sp+cp), y0 + l1*(cp-sp), h1 #enlarging to take acount of the rotation angle
+    refBox2_minx, refBox2_miny, refBox2_minz = \
+        x0 - l2*(sp+cp), y0 - l2*(cp-sp), 0 #enlarging to take acount of the rotation angle
+    refBox2_maxx, refBox2_maxy, refBox2_maxz = \
+        x0 + l2*(sp+cp), y0 + l2*(cp-sp),h2 #enlarging to take acount of the rotation angle
+    
+    # changing snappyHexMeshDict - with parsedParameterFile
+    SHMDict = ParsedParameterFile(
+        path.join(work.systemDir(), "snappyHexMeshDict"))
+    SHMDict["geometry"]["refinementBox1"]["min"] = \
+        "("+str(refBox1_minx)+" "+str(refBox1_miny)+" "+str(refBox1_minz)+")"
+    SHMDict["geometry"]["refinementBox1"]["max"] = \
+        "("+str(refBox1_maxx)+" "+str(refBox1_maxy)+" "+str(refBox1_maxz)+")"
+    SHMDict["geometry"]["refinementBox2"]["min"] = \
+        "("+str(refBox2_minx)+" "+str(refBox2_miny)+" "+str(refBox2_minz)+")"
+    SHMDict["geometry"]["refinementBox2"]["max"] = \
+        "("+str(refBox2_maxx)+" "+str(refBox2_maxy)+" "+str(refBox2_maxz)+")"
+    SHMDict["castellatedMeshControls"]["locationInMesh"] = "("+str(x0)+" "+str(y0)+" "+str(zz)+")"
+    levelRef = SHM["cellSize"]["levelRef"]
+    SHMDict["castellatedMeshControls"]["refinementSurfaces"]["terrain"]["level"] = \
+        "("+str(levelRef)+" "+str(levelRef)+")"
+    r = SHM["cellSize"]["r"]
+    SHMDict["addLayersControls"]["expansionRatio"] = r
+    fLayerRatio = SHM["cellSize"]["fLayerRatio"]
+    SHMDict["addLayersControls"]["finalLayerThickness"] = fLayerRatio
+    # calculating finalLayerRatio for getting 
+    zp_z0 = SHM["cellSize"]["zp_z0"]
+    firstLayerSize = 2 * zp_z0 * z0
+    L = log(fLayerRatio/firstLayerSize*cell/2**levelRef) / log(r) + 1
+    SHMDict["addLayersControls"]["layers"]["terrain_solid"]["nSurfaceLayers"] = int(round(L))
+    SHMDict.writeFile()
+
+def create_boundary_conditions_dict(work, wind_dict, params):
+    #--------------------------------------------------------------------------------------
+    # changing inlet profile - - - - according to Martinez 2010
+    #--------------------------------------------------------------------------------------
+    phi = params['phi']
+    SHM = wind_dict['SHMParams']
+    kEpsParams = wind_dict['kEpsParams']
+    UM = wind_dict['simParams']['UM']
+    yM = wind_dict['simParams']['yM']
+    k = kEpsParams['k'] # von karman constant
+    z0 = wind_dict['simParams']['z0'] # TODO: calculated per wind direction using roughness2foam
+    us = UM * k / log(yM / z0)
+    Href = SHM['domainSize']['domZ']
+    Cmu = kEpsParams['Cmu'] # K-epsilon turbulence closure parameter
+    # change inlet profile
+    Uref = Utop = us / k * log(Href / z0)
+    # calculating turbulentKE
+    TKE = us * us / sqrt(Cmu)
+    # 1: changing ABLConditions
+    bmName = path.join(work.initialDir(),"include", "ABLConditions")
+    template = TemplateFile(bmName + ".template")
+    template.writeToFile(bmName,
+        {'us':us,'Uref':Uref,'Href':Href,'z0':z0,
+         'xDirection':sin(phi),'yDirection':cos(phi)})
+    # 2: changing initialConditions
+    bmName = path.join(work.initialDir(),"include", "initialConditions")
+    template = TemplateFile(bmName + ".template")
+    template.writeToFile(bmName,{'TKE':TKE})
+    # 3: changing initial and boundary conditions for new z0
+    # changing ks in nut, inside nutRoughWallFunction
+    nutFile = ParsedParameterFile(path.join(work.initialDir(), "nut"))
+    nutFile["boundaryField"]["ground"]["z0"].setUniform(z0)
+    nutFile["boundaryField"]["terrain_.*"]["z0"].setUniform(z0)
+    nutFile.writeFile()
+
+def read_dict_string(d, key):
+    """
+    to allow using a filename like so:
+    template "/home/hanan/bin/OpenFOAM/windpyfoam/test_template";
+
+    if you remove the '"' you get an Illegal '/' used message
+    """
+    val = d[key]
+    if len(val) > 0 and val[0] == '"':
+        assert(val[-1] == '"')
+        val = val[1:-1]
+    return val
+
+def create_case(wind_dict, params):
+    """
+     0. cloning case
+     1. creating snappyHexMeshDict and blockMeshdict according to flow direction and other parameters
+     2. creating the blockMesh
+     3. change the boundary conditions
+     4. decomposing the domain
+     5. creating the snappyHexMesh - running in parallel (sfoam.py or not - depending on user input)
+     6. decomposing the created mesh
+    """
+    #--------------------------------------------------------------------------------------
+    # cloning case
+    #--------------------------------------------------------------------------------------
+    target = params['case_dir']
+    template = read_dict_string(wind_dict, 'template')
+    debug("template = %r" % template)
+    orig = SolutionDirectory(template,
+                             archive=None,
+                             paraviewLink=False)
+    work = orig.cloneCase(target)
+
+    #--
+    # creating dictionaries
+    #--
+    if wind_dict['procnr'] > multiprocessing.cpu_count():
+        warn('wind_dict contains a higher processor number then the machine has')
+        wind_dict['procnr'] = min(wind_dict['procnr'], multiprocessing.cpu_count())
+    phi = params['wind_dir'] * pi / 180
+    params['phi'] = phi - pi/180 * 90
+
+    status('creating block mesh dictionary')
+    create_block_mesh_dict(work, wind_dict, params)
+    status('creating snappy hex mesh dictionary')
+    create_SHM_dict(work, wind_dict, params)
+    status('creating boundary conditions dictionary')
+    create_boundary_conditions_dict(work, wind_dict, params)
+    status('running block mesh')
+    run_block_mesh(work)
+    status('running decompose')
+    run_decompose(work, wind_dict)
+    status('running snappy hex mesh')
+    run_SHM(work)
+    status('running second decompose')
+    run_decompose(work, wind_dict)
+    return work
+
+def run_decompose(work, wind_dict):
+    Decomposer(args=[work.name, wind_dict['procnr']])
+
+def run_block_mesh(work):
+    blockRun = BasicRunner(argv=["blockMesh", '-case', work.name],
+                           silent=True, server=False, logname="blockMesh")
+    print "Running blockMesh"
+    blockRun.start()
+    if not blockRun.runOK(): error("there was an error with blockMesh")
+
+def run_SHM(work):
+    # TODO - add parallel runs!
+    SHMrun = BasicRunner(argv=["snappyHexMesh",'-overwrite','-case',work.name],
+                         server=False,logname="SHM")
+    print "Running SHM"
+    SHMrun.start()
+
+def grid_convergance_params_generator(wind_dict):
+    """
+    yields names of case directories
+    """
+    grid_convergence = wind_dict["caseTypes"]["gridConvergenceParams"]
+    gridRange = grid_convergence['gridRange']
+    template = read_dict_string(wind_dict, 'template')
+    wind_dir = grid_convergence['windDir']
+    for i, cell_size in enumerate(gridRange):
+        case_dir = os.path.join('runs','%(template)s_%(cell_size)s' % locals())
+        yield dict(case_dir=case_dir, wind_dir=wind_dir, cell_size=cell_size,
+                   name='grid_convergence %d: cell_size=%d, wind_dir=%d' % (i, cell_size, wind_dir))
+
+def wind_rose_params_generator(wind_dict):
+    """
+    yields names of case directories
+    one for each direction from wind rose
+    """
+    windRose = wind_dict["windRose"]
+    template = wind_dict['template']
+    cell_size = wind_dict['cell_size']
+    for i, (_weight, wind_dir) in enumerate(windRose):
+        case_dir = os.path.join('runs', '%(template)s_%(wind_dir)s' % locals())
+        yield dict(case_dir = case_dir, wind_dir = wind_dir, cell_size = cell_size,
+                   name='wind_rose %d: cell_size=%d, wind_dir=%d' % (i, cell_size, wind_dir))
+
+def main(conf):
+    """
+    Mesh: creating the write snappyHexMeshDict file
+    use the right snappyHexMeshDict_XXX.template file with wind_dict
+     but --> already at this stage i have to work in a loop according to the
+     amount of cases i am asked to solve. each case will be cloned from the
+     template case, and then the procedure of
+     if doing grid convergance with a single 
+     foreach dir in {winddirs, grid}:
+         1. creating snappyHexMeshDict and blockMeshdict according to flow direction and other parameters
+         2. creating the blockMesh
+         3. decomposing the domain
+         4. creating the snappyHexMesh - running in parallel (sfoam.py or not - depending on user input)
+         5. decomposing the created mesh
+         6. running pyFoamRunner.py through sfoam (or not - depending on user input)
+
+     After all cases stop running
+     7. if exist (usually) - reading real measurements
+     8. creating sampleDict according to measurement locations and user input
+        (which asks for wind speed contour map at certain height above ground)
+     9. sampling (command is "sample")
+     10. using the results to calculate the following metrics
+        1. for a specific grid size and multiple directions and weights for
+           each direction - the power density and average wind speed averaged over
+           the direction at specific heights above ground level
+        2. for a specific direction and different grid cell sizes - the grid
+           error according to some known grid convergence algorithm
+        3. a "hit rate" which shows the aggreement of the simulated wind speeds
+           and turbulence to the measurements
+    """
+    if not os.path.exists(conf):
+        print "missing %s file" % conf
         raise SystemExit
     try:
-        ParsedParameterFile(wind_dict)
+        wind_dict = ParsedParameterFile(conf)
     except Exception, e:
         print "failed to parse windPyFoam parameter file:"
         print e
         raise SystemExit
+    # running the grid convergence routine - for 1 specific direction
+    if wind_dict["caseTypes"]["gridConvergence"]:
+        gen = grid_convergance_params_generator(wind_dict)
+    gen = itertools.chain(gen,
+            wind_rose_params_generator(wind_dict))
+    cases = []
+    for params in gen:
+        debug(params['name'])
+        work = create_case(wind_dict, params)
+        cases.append(work)
+    # TODO: customizable runArg
+    runCases(n=wind_dict['procnr'], runArg='Runner',
+             cases=[case.name for case in cases])
+
+# 5/
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('--case', required=True)
+    parser.add_argument('--conf', required=True)
     args = parser.parse_args(sys.argv[1:])
-    main(args.case)
+    main(args.conf)
